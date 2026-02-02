9623f6f227a799a4f64bd0d9f034d394a250474a
phuonganh <phuonganhnguyenomg@gmail.com>
2026-02-02 14:45:25 +0100

    change import from useRouter

--- src/app/components/FeedbackForm.tsx
+++ src/app/components/FeedbackForm.tsx
@@ -1,7 +1,7 @@
 "use client"
 import {useState} from "react"
 import { setTimeout } from "timers/promises";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 export default function FeedbackForm () {
   const router=useRouter()
   const [type,setType]=useState("")
